d3d0e684a8d2cd73db6c06862ec162d4107316a8
lestavelbaptiste <baptiste.lestavel@hei.fr>
2019-02-14 15:52:36 +0100

    try fix issue with css.

--- client/src/components/commun/Card/index.js
+++ client/src/components/commun/Card/index.js
@@ -4,7 +4,7 @@ import propTypes from 'prop-types';
 
 const Card = props => {
   return (
-    <div className={css.component}>
+    <div className='Card'>
       <h2 className="title">{props.title}</h2>
       {props.children}
     </div>
